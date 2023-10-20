31c8cd2b33558d6343f47956fcff13f733320dda
Robert Haranguș <robert.harangus@gmail.com>
2023-10-20 12:49:42 +0300

    Lab 2 Work

--- Lexical Analyzer/SymbolTable.py
+++ Lexical Analyzer/SymbolTable.py
@@ -1,40 +1,22 @@
 class SymbolTable:
     def __init__(self, size):
         self.size = size
-        self.table = [None] * size
+        self.data = [[] for _ in range(size)]
 
     def _hash_function(self, key):
-        return hash(key) % self.size
+        ## TODO: think of a good hash
+        pass
 
-    def insert(self, key, value):
+    def insert(self, key):
         index = self._hash_function(key)
-        if self.table[index] is None:
-            self.table[index] = []
-        self.table[index].append((key, value))
+        if self.data[index] is None:
+            self.data[index] = []
+        self.data[index].append(key)
 
     def get(self, key):
-        index = self._hash_function(key)
-        if self.table[index] is not None:
-            for k, v in self.table[index]:
-                if k == key:
-                    return v
-        raise KeyError(f"Key '{key}' not found")
+        ## TODO: implementation
+        pass
 
     def delete(self, key):
-        index = self._hash_function(key)
-        if self.table[index] is not None:
-            for item in self.table[index]:
-                if item[0] == key:
-                    self.table[index].remove(item)
-                    return
-        raise KeyError(f"Key '{key}' not found")
-
-# Example usage:
-hash_table = HashTable(10)
-hash_table.insert("apple", 5)
-hash_table.insert("banana", 2)
-hash_table.insert("cherry", 8)
-
-print(hash_table.get("apple"))  # Output: 5
-
-hash_table.delete("banana")
+        ## TODO: implementation
+        pass
